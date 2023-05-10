09c30235b08779b16f773e6fc366a61047763197
eldar mutaev <eldarmutaev@inbox.ru>
2023-05-10 22:20:59 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import { Routes, Route } from 'react-router-dom'
+import Container from "@mui/material/Container";
+import { useDispatch, useSelector } from 'react-redux'
+
+import { Header } from "./components";
+import React from 'react'
+import { Home, FullPost, Registration, AddPost, Login } from "./pages";
+import {fetchAuthMe, selectIsAuth} from "./redux/slices/auth";
+
+function App() {
+    const dispatch = useDispatch();
+    const isAuth = useSelector(selectIsAuth)
+
+    React.useEffect(() => {
+        dispatch(fetchAuthMe())
+    }, []);
+
+  return (
+    <>
+      <Header />
+      <Container maxWidth="lg">
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/posts/:id" element={<FullPost />} />
+            <Route path="/add-post" element={<AddPost />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Registration />} />
+        </Routes>
+      </Container>
+    </>
+  );
+}
+
+export default App;
